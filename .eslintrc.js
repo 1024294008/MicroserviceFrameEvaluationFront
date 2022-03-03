7fb9c8918d8c092bb58a335f0d22ce204dea129d
hupeng <13515616243@163.com>
2022-03-03 17:06:16 +0800

    feat: init project

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "rules": {
+    "semi": [2, "never"]
+  },
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module"
+  },
+  "parser": "vue-eslint-parser",
+}
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import DetectTask from '../pages/detectTask/DetectTask'
+import DetectDetail from '../pages/detectDetail/DetectDetail'
+
+const routes = [
+  {
+    path: '/',
+    component: DetectTask
+  },
+  {
+    path: '/detail',
+    component: DetectDetail
+  }
+]
+
+export default createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
